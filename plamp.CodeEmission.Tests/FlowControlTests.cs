a9d8991ab3281a067bc3cb4fb4a588d37b772f80
romik <ligma@balls.sula>
2025-06-17 00:14:45 +0500

    flow control test added
    + flow control tests
    + increased readability of debug method builder listings

--- plamp.CodeEmission.Tests/FlowControlTests.cs
+++ plamp.CodeEmission.Tests/FlowControlTests.cs
@@ -69,4 +69,150 @@ public class FlowControlTests
         var res = method!.Invoke(instance, [argValue]);
         Assert.Equal(resShould, res);
     }
+
+    [Fact]
+    public async Task InsideContinueDoesNotAffectsOutside()
+    {
+        const string methodName = "DoubleLoop";
+        var (_, typeBuilder, methodBuilder, _) =
+            EmissionSetupHelper.CreateMethodBuilder(methodName, typeof(int), []);
+        
+        /*
+         * int i = 0
+         * int k = 0
+         * while(i < 10)
+         *     int j = 0
+         *     while(j < 10)
+         *         j = j + 1
+         *         continue
+         *         k = k + 1
+         *     i = i + 1
+         * return i + k
+         */
+        bool i, j, k;
+        const int iterationCount = 10;
+        var body = new BodyNode(
+        [
+            new AssignNode(new VariableDefinitionNode(EmissionSetupHelper.CreateTypeNode(typeof(int)), new MemberNode(nameof(i))), new LiteralNode(0, typeof(int))),
+            new AssignNode(new VariableDefinitionNode(EmissionSetupHelper.CreateTypeNode(typeof(int)), new MemberNode(nameof(k))), new LiteralNode(0, typeof(int))),
+            new WhileNode(new LessNode(new MemberNode(nameof(i)), new LiteralNode(iterationCount, iterationCount.GetType())),
+                new BodyNode(
+                [
+                    new AssignNode(new VariableDefinitionNode(EmissionSetupHelper.CreateTypeNode(typeof(int)), new MemberNode(nameof(j))), new LiteralNode(0, typeof(int))),
+                    new WhileNode(
+                        new LessNode(new MemberNode(nameof(j)), new LiteralNode(iterationCount, iterationCount.GetType())),
+                        new BodyNode(
+                        [
+                            new AssignNode(new MemberNode(nameof(j)), new PlusNode(new MemberNode(nameof(j)), new LiteralNode(1, typeof(int)))),
+                            new ContinueNode(),
+                            new AssignNode(new MemberNode(nameof(k)), new PlusNode(new MemberNode(nameof(k)), new LiteralNode(1, typeof(int))))
+                        ])),
+                    new AssignNode(new MemberNode(nameof(i)), new PlusNode(new MemberNode(nameof(i)), new LiteralNode(1, typeof(int))))
+                ])),
+            new ReturnNode(new PlusNode(new MemberNode(nameof(i)), new MemberNode(nameof(k))))
+        ]);
+        var context = new CompilerEmissionContext(body, methodBuilder, [], null, null);
+        var emitter = new DefaultIlCodeEmitter();
+        await emitter.EmitMethodBodyAsync(context, CancellationToken.None);
+
+        var type = typeBuilder.CreateType();
+        var (instance, method) = EmissionSetupHelper.CreateObject(type, methodName);
+        var res = method!.Invoke(instance!, []);
+        Assert.Equal(iterationCount, res);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(100)]
+    [InlineData(1000)]
+    public async Task BreakOnIteration(int iterationNumber)
+    {
+        const string methodName = "IterationBreak";
+        var argType = typeof(int);
+        var (_, typeBuilder, methodBuilder, _) =
+            EmissionSetupHelper.CreateMethodBuilder(methodName, typeof(int), [argType]);
+        var arg = new TestParameter(argType, "n");
+        
+        /*
+         * var i = 0
+         * while(true)
+         *     if(i == n)
+         *         break
+         *     i = i + 1
+         * return i
+         */
+        bool i;
+        var body = new BodyNode(
+        [
+            new AssignNode(new VariableDefinitionNode(EmissionSetupHelper.CreateTypeNode(typeof(int)), new MemberNode(nameof(i))), new LiteralNode(0, typeof(int))),
+            new WhileNode(new LiteralNode(true, typeof(bool)),
+                new BodyNode(
+                [
+                    new ConditionNode(
+                        new EqualNode(new MemberNode(nameof(i)), new MemberNode(arg.Name)),
+                        new BodyNode(
+                        [
+                            new BreakNode()
+                        ]), null),
+                    new AssignNode(new MemberNode(nameof(i)), new PlusNode(new MemberNode(nameof(i)), new LiteralNode(1, typeof(int))))
+                ])),
+            new ReturnNode(new MemberNode(nameof(i)))
+        ]);
+        var emissionContext = new CompilerEmissionContext(body, methodBuilder, [arg], null, null);
+        var emitter = new DefaultIlCodeEmitter();
+        await emitter.EmitMethodBodyAsync(emissionContext, CancellationToken.None);
+
+        var type = typeBuilder.CreateType();
+        var (instance, method) = EmissionSetupHelper.CreateObject(type, methodName);
+        var res = method!.Invoke(instance!, [iterationNumber]);
+        Assert.Equal(iterationNumber, res);
+    }
+
+    [Fact]
+    public async Task InnerBreakDoesNotBreakOuter()
+    {
+        const string methodName = "innerBreak";
+        var (_, typeBuilder, methodBuilder, _) = EmissionSetupHelper.CreateMethodBuilder(methodName, typeof(int), []);
+        
+        /*
+         * int i = 0
+         * while(i < 10)
+         *     int j = 0
+         *     while(true)
+         *         break
+         *         j = j + 1
+         *     i = i + 1
+         * return i + j
+         */
+        const int iterationCount = 10;
+        bool i, j;
+        var body = new BodyNode(
+        [
+            new AssignNode(new VariableDefinitionNode(EmissionSetupHelper.CreateTypeNode(typeof(int)), new MemberNode(nameof(i))), new LiteralNode(0, typeof(int))),
+            new AssignNode(new VariableDefinitionNode(EmissionSetupHelper.CreateTypeNode(typeof(int)), new MemberNode(nameof(j))), new LiteralNode(0, typeof(int))),
+            new WhileNode(
+                new LessNode(new MemberNode(nameof(i)), new LiteralNode(iterationCount, typeof(int))),
+                new BodyNode(
+                [
+                    new WhileNode(new LiteralNode(true, typeof(bool)),
+                        new BodyNode(
+                        [
+                            new BreakNode(),
+                            new AssignNode(new MemberNode(nameof(j)), new PlusNode(new MemberNode(nameof(j)), new LiteralNode(1, typeof(int))))
+                        ])),
+                    new AssignNode(new MemberNode(nameof(i)), new PlusNode(new MemberNode(nameof(i)), new LiteralNode(1, typeof(int))))
+                ])),
+            new ReturnNode(new PlusNode(new MemberNode(nameof(i)), new MemberNode(nameof(j))))
+        ]);
+        var context = new CompilerEmissionContext(body, methodBuilder, [], null, null);
+        var emitter = new DefaultIlCodeEmitter();
+        await emitter.EmitMethodBodyAsync(context, CancellationToken.None);
+
+        var type = typeBuilder.CreateType();
+        var (instance, method) = EmissionSetupHelper.CreateObject(type, methodName);
+
+        var res = method!.Invoke(instance!, []);
+        Assert.Equal(iterationCount, res);
+    }
 }
--- plamp.CodeEmission.Tests/Infrastructure/DebugILGenerator.cs
+++ plamp.CodeEmission.Tests/Infrastructure/DebugILGenerator.cs
@@ -9,12 +9,12 @@ public class DebugILGenerator : ILGenerator
 {
     private readonly ILGenerator _inner;
     private int _varScopeCounter;
-    private int _pos;
 
     //Не билдер так как нужно менять последнюю строку.
     private readonly StringBuilder _code = new();
     private readonly List<LocalBuilder> _locals = [];
     private readonly List<Label> _labels = [];
+    private readonly List<string> _unwrittenLabels = [];
 
     internal DebugILGenerator(ILGenerator inner)
     {
@@ -35,7 +35,6 @@ public class DebugILGenerator : ILGenerator
     {
         _code.AppendLine($"BEGIN LEXICAL SCOPE {_varScopeCounter++}");
         _inner.BeginScope();
-        FinishStatement();
     }
 
     public override LocalBuilder DeclareLocal(Type localType, bool pinned)
@@ -43,7 +42,6 @@ public class DebugILGenerator : ILGenerator
         _code.AppendLine($"DECLARE LOCAL {_locals.Count} {localType.Name} PINNED: {pinned}");
         var local = _inner.DeclareLocal(localType, pinned);
         _locals.Add(local);
-        FinishStatement();
         return local;
     }
 
@@ -56,182 +54,163 @@ public class DebugILGenerator : ILGenerator
 
     public override void Emit(OpCode opcode)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         _code.AppendLine(opcode.ToString() ?? throw new InvalidOperationException());
         _inner.Emit(opcode);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, byte arg)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} {arg}");
         _inner.Emit(opcode, arg);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, double arg)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} {arg}");
         _inner.Emit(opcode, arg);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, short arg)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} {arg}");
         _inner.Emit(opcode, arg);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, int arg)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} {arg}");
         _inner.Emit(opcode, arg);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, long arg)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} {arg}");
         _inner.Emit(opcode, arg);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, ConstructorInfo con)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} {con.Name}({string.Join(", ", con.GetParameters().Select(x => x.ParameterType.Name))})");
         _inner.Emit(opcode, con);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, Label label)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} lab_{_labels.IndexOf(label)}");
         _inner.Emit(opcode, label);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, Label[] labels)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         var labConcat = string.Join(' ', labels.Select(x => $"lab_{_labels.IndexOf(x)}"));
         _code.AppendLine($"{code} {labConcat}");
         _inner.Emit(opcode, labels);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, LocalBuilder local)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} loc_{_locals.IndexOf(local)}");
         _inner.Emit(opcode, local);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, SignatureHelper signature)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} {signature}");
         _inner.Emit(opcode, signature);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, FieldInfo field)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} {field.DeclaringType}.{field.Name}");
         _inner.Emit(opcode, field);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, MethodInfo meth)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         var infoStr = GetMethodInfoStringDesc(meth);
         _code.AppendLine($"{code} {infoStr}");
         _inner.Emit(opcode, meth);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, float arg)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} {arg}");
         _inner.Emit(opcode, arg);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, string str)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} \"{str}\"");
         _inner.Emit(opcode, str);
-        FinishStatement();
     }
 
     public override void Emit(OpCode opcode, Type cls)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         _code.AppendLine($"{code} {cls.Name}");
         _inner.Emit(opcode, cls);
-        FinishStatement();
     }
 
     public override void EmitCall(OpCode opcode, MethodInfo methodInfo, Type[]? optionalParameterTypes)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         var opt = optionalParameterTypes == null ? string.Empty : string.Join(' ', optionalParameterTypes.Select(x => x.Name));
         var infoString = GetMethodInfoStringDesc(methodInfo);
         _code.AppendLine($"{code} {infoString}{(opt == string.Empty ? opt : " " + opt)}");
         _inner.EmitCall(opcode, methodInfo, optionalParameterTypes);
-        FinishStatement();
     }
 
     public override void EmitCalli(OpCode opcode, CallingConventions callingConvention, Type? returnType, Type[]? parameterTypes,
         Type[]? optionalParameterTypes)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         var args = parameterTypes?.Concat(optionalParameterTypes ?? []) ?? optionalParameterTypes;
         var opt = args == null ? string.Empty : string.Join(' ', args.Select(x => x.Name));
         var ret = returnType == null ? string.Empty : returnType.Name;
         _code.AppendLine($"{code} {callingConvention} RET: {ret} ARGS: {(opt == string.Empty ? opt : " " + opt)}");
         _inner.EmitCalli(opcode, callingConvention, returnType, parameterTypes, optionalParameterTypes);
-        FinishStatement();
     }
 
     public override void EmitCalli(OpCode opcode, CallingConvention unmanagedCallConv, Type? returnType, Type[]? parameterTypes)
     {
-        AddLeadingSpaces();
+        WritePrefix();
         var code = opcode.ToString() ?? throw new InvalidOperationException();
         var opt = parameterTypes == null ? string.Empty : string.Join(' ', parameterTypes.Select(x => x.Name));
         var ret = returnType == null ? string.Empty : returnType.Name;
         _code.AppendLine($"{code} {unmanagedCallConv} RET: {ret} ARGS: {(opt == string.Empty ? opt : " " + opt)}");
         _inner.EmitCalli(opcode, unmanagedCallConv, returnType, parameterTypes);
-        FinishStatement();
     }
 
     public override void EndExceptionBlock() => throw new NotImplementedException();
@@ -240,16 +219,12 @@ public class DebugILGenerator : ILGenerator
     {
         _code.AppendLine($"END LEXICAL SCOPE {--_varScopeCounter}");
         _inner.EndScope();
-        FinishStatement();
     }
 
     public override void MarkLabel(Label loc)
     {
-        var ix = _labels.IndexOf(loc);
-        var labStr = $"lab_{ix}:";
-        _pos += labStr.Length;
-        _code.Append(labStr);
         _inner.MarkLabel(loc);
+        _unwrittenLabels.Add($"lab_{_labels.IndexOf(loc)}:");
     }
 
     public override void UsingNamespace(string usingNamespace) => throw new NotImplementedException();
@@ -258,13 +233,19 @@ public class DebugILGenerator : ILGenerator
 
     public override string ToString() => _code.ToString();
 
-    private void AddLeadingSpaces()
+    private void WritePrefix()
     {
-        var spaceCount = Math.Max(10 - _pos, 0);
-        _code.Append(' ', spaceCount);
+        if (_unwrittenLabels.Count != 0)
+        {
+            foreach (var label in _unwrittenLabels)
+            {
+                _code.AppendLine(label);
+            }
+            _unwrittenLabels.Clear();
+        }
+        
+        _code.Append(' ', 6);
     }
-
-    private void FinishStatement() => _pos = 0;
 
     private string GetMethodInfoStringDesc(MethodInfo meth)
     {
