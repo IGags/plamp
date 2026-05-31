32e817412a4c13879bc4b38eb5ad500c0feeb720
romik <ligma@balls.sula>
2026-05-31 23:29:01 +0500

    another review and conflict fixes

--- plamp.end-to-end.Tests/Tests/DiagnosticsTests.cs
+++ plamp.end-to-end.Tests/Tests/DiagnosticsTests.cs
@@ -56,7 +56,7 @@ public class SyntaxDiagnosticsTests
         { InvalidFile("InvalidAssignCountMismatch.plp"), PlampExceptionInfo.AssignSourceAndTargetCountMismatch().Code },
         { InvalidFile("InvalidCallArgumentCount.plp"), PlampExceptionInfo.FunctionHasDifferentArgCount(2, 1).Code },
         { InvalidFile("InvalidUnclosedMultiLineComment.plp"), PlampExceptionInfo.CommentIsNotClosed().Code },
-        { InvalidFile("InvalidUnclosedChar.plp"), PlampExceptionInfo.ExpectedEndOfStatement().Code },
+        { InvalidFile("InvalidUnclosedChar.plp"), PlampExceptionInfo.CharIsNotClosed().Code },
         { InvalidFile("InvalidEmptyChar.plp"), PlampExceptionInfo.InvalidCharLiteral().Code },
         { InvalidFile("InvalidLongChar.plp"), PlampExceptionInfo.InvalidCharLiteral().Code },
         { InvalidFile("InvalidCharEscape.plp"), PlampExceptionInfo.InvalidEscapeSequence("\\x").Code },
